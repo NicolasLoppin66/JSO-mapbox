3a530dc791123df1456ea001344140d66bd4a0ce
NicolasLoppin66 <loppin.n66@gmail.com>
2023-03-02 16:35:01 +0100

    Chargement de la carte

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,44 @@
 import Config from '../../app.config.json';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import '../Assets/style.css';
+
+import mapboxGl from 'mapbox-gl';
 
 class App {
 
+    /**
+     * <div> de la map mapbox
+     */
+    elDivMap;
+
+    /**
+     * Instance de la map mapbox
+     */
+    map;
+
     start() {
         console.log('App démarrer ...');
-        console.log('Votre clé API mapbox : ', Config.api.mapbox_gl.Api_Key);
+
+        this.initDom();
+        this.initMapbox();
+    }
+
+    initDom() {
+        this.elDivMap = document.createElement('div');
+        this.elDivMap.id = 'map';
+
+        document.body.append(this.elDivMap);
+    }
+
+    initMapbox() {
+        mapboxGl.accessToken = Config.api.mapbox_gl.Api_Key;
+
+        this.map = new mapboxGl.Map({
+            container: this.elDivMap,
+            style: Config.api.mapbox_gl.Map_Styles.streets,
+            center: { lng: 2.213749, lat: 46.227638 },
+            zoom: 5.5
+        });
     }
 }
 
